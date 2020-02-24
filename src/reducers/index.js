e7ec7315d1e3d6be891f019d2c41a3d2d13c460e
nick stern <nicholas.stern2@gmail.com>
2020-02-24 06:48:48 -0500

    redux-form setup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
-
+// form key needs to be exact (form doc)
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
