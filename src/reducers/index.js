60598522ef5c66f37fd222f5cfb3469e8af22224
nick stern <nicholas.stern2@gmail.com>
2020-02-27 12:39:39 -0500

    actions/reducers set

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 // form key needs to be exact (form doc)
 export default combineReducers({
   auth: authReducer,
-  form: formReducer
+  form: formReducer,
+  streams: streamReducer
 });
