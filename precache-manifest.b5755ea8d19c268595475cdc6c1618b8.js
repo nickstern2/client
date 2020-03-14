503fbecfc2378a5cc038db1ff42d8e1c06c8c810
nick stern <nicholas.stern2@gmail.com>
2020-03-14 17:42:40 -0400

    Updates

---
+++ precache-manifest.b5755ea8d19c268595475cdc6c1618b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a2b5d51796ce44b3608d21885edd33",
+    "url": "/twitch/index.html"
+  },
+  {
+    "revision": "573811347631e123f206",
+    "url": "/twitch/static/js/2.120fe838.chunk.js"
+  },
+  {
+    "revision": "c0f4aa119942e5c1f85dd4acb785bdb8",
+    "url": "/twitch/static/js/2.120fe838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72aa5e8bca92ca025eb",
+    "url": "/twitch/static/js/main.fe12c8cb.chunk.js"
+  },
+  {
+    "revision": "a474fb27351e4344e9f8",
+    "url": "/twitch/static/js/runtime-main.8ca7a7e1.js"
+  }
+]);
